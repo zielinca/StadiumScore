bfebc3708339a6ecbab806833c13cb5fe2812a05
Carter Zielinski <zielinca@bc.edu>
2026-04-22 11:57:11 -0400

    Initial Commit

---
+++ StadiumScore/StadiumScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StadiumScoreApp.swift
+//  StadiumScore
+//
+//  Created by Carter Zielinski on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct StadiumScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
